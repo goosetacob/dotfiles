c2acc1649d98b8dfb92c6f34248325c30a6f931e
Gustavo Blanco <gustavo.jr.blanco@gmail.com>
2021-06-19 13:41:38 -0700

    clean lsp.lua

--- nvim/lua/goosetacob/lsp.lua
+++ nvim/lua/goosetacob/lsp.lua
@@ -1,21 +1,30 @@
-local on_attach = require'completion'.on_attach
+-- Docs: https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md
+local lspconfig = require'lspconfig'
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+local on_attach = function(client)
+	require'completion'.on_attach(client)
+end
 
--- https://github.com/neovim/nvim-lspconfig/tree/master/lua/lspconfig
-require'lspconfig'.tsserver.setup{ on_attach=on_attach }
-require'lspconfig'.gopls.setup{ on_attach=on_attach }
--- require'lspconfig'.pyls.setup{ on_attach=require'completion'.on_attach }
-require'lspconfig'.rust_analyzer.setup{ on_attach=on_attach }
-require'lspconfig'.svelte.setup{ on_attach=on_attach }
-require'lspconfig'.terraformls.setup{ on_attach=on_attach }
-require'lspconfig'.bashls.setup{ on_attach=on_attach }
-require'lspconfig'.cssls.setup{
+lspconfig.tsserver.setup{ on_attach=on_attach }
+
+lspconfig.gopls.setup{ on_attach=on_attach }
+
+lspconfig.rust_analyzer.setup{ on_attach=on_attach }
+
+lspconfig.svelte.setup{ on_attach=on_attach }
+
+lspconfig.terraformls.setup{ on_attach=on_attach }
+
+lspconfig.bashls.setup{ on_attach=on_attach }
+
+lspconfig.cssls.setup{
 	on_attach=on_attach,
 	capabilities = capabilities,
 }
-require'lspconfig'.html.setup{
+
+lspconfig.html.setup{
 	on_attach=on_attach,
 	capabilities = capabilities,
 }
