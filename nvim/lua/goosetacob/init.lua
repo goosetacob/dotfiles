fd99497db36c2660a5ad906e5c47128ce7df63ed
Gustavo Blanco <gustavo.jr.blanco@gmail.com>
2021-10-27 11:59:03 -0700

    remove custom diagnostic characters

--- nvim/lua/goosetacob/init.lua
+++ nvim/lua/goosetacob/init.lua
@@ -3,24 +3,14 @@
 -- https://github.com/nanotee/nvim-lua-guide
 -- https://github.com/svermeulen/vimpeccable
 
-local lsp = vim.lsp
-local fn = vim.fn
-
-lsp.handlers["textDocument/publishDiagnostics"] =
-	lsp.with(
-		lsp.diagnostic.on_publish_diagnostics,
-		{
-			underline = true,
-			virtual_text = false,
-			signs = true,
-			update_in_insert = true,
-		}
-	)
-
-fn.sign_define("LspDiagnosticsSignError", { text = "✖", numhl = "LspDiagnosticsDefaultError" })
-fn.sign_define("LspDiagnosticsSignWarning", { text = "▲", numhl = "LspDiagnosticsDefaultWarning" })
-fn.sign_define("LspDiagnosticsSignInformation", { text = "⁈", numhl = "LspDiagnosticsDefaultInformation" })
-fn.sign_define("LspDiagnosticsSignHint", { text = "❖", numhl = "LspDiagnosticsDefaultHint" })
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+	vim.lsp.diagnostic.on_publish_diagnostics, {
+		underline = true,
+		virtual_text = false,
+		signs = true,
+		update_in_insert = true,
+	}
+)
 
 vim.o.termguicolors = true
 
