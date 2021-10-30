718444b4c0839d66213c4275db6c916338392c8a
Gustavo Blanco <gustavo.jr.blanco@gmail.com>
2021-10-29 21:05:24 -0700

    remove stale bin files + karabiner config stale

--- bin/build-rust-analyzer.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-LOCAL_BIN_DIR=$HOME/.local/bin
-mkdir -p $LOCAL_BIN_DIR
-pushd $LOCAL_BIN_DIR
-git clone https://github.com/rust-analyzer/rust-analyzer.git
-pushd rust-analyzer
-cargo xtask install --server
-pushd +0
