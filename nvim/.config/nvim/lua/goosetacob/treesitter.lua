2fb41437f90451d387a02ed41f569f9e5c005343
Gustavo Blanco <gustavo.jr.blanco@gmail.com>
2022-05-14 12:20:42 -0700

    ts ensure_installed all

--- nvim/.config/nvim/lua/goosetacob/treesitter.lua
+++ nvim/.config/nvim/lua/goosetacob/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = 'maintained',
+	ensure_installed = 'all',
 	highlight = {enable = true, use_languagetree = true},
 	indent = {enable = true},
 	rainbow = {enable = true, extended_mode = true, max_file_lines = 1000},
