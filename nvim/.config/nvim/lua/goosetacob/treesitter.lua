03e3362d5c85f1a58f08a7a2a9c8daf9b06d2069
Gustavo Blanco <gustavo.jr.blanco@gmail.com>
2022-05-14 15:49:33 -0700

    skip installing phpdoc

--- nvim/.config/nvim/lua/goosetacob/treesitter.lua
+++ nvim/.config/nvim/lua/goosetacob/treesitter.lua
@@ -1,5 +1,6 @@
 require'nvim-treesitter.configs'.setup {
 	ensure_installed = 'all',
+	ignore_install = {'phpdoc', 'tree-sitter-phpdoc'},
 	highlight = {enable = true, use_languagetree = true},
 	indent = {enable = true},
 	rainbow = {enable = true, extended_mode = true, max_file_lines = 1000},
