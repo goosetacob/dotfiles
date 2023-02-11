11fec0ac08bc02bce6ec4cdd06b06f61ad4c249b
Gustavo Blanco <gustavo.jr.blanco@gmail.com>
2023-02-11 11:36:06 -0800

    again more fmt???

--- nvim/.config/nvim/lua/goosetacob/autocmd.lua
+++ nvim/.config/nvim/lua/goosetacob/autocmd.lua
@@ -2,12 +2,12 @@
 -- https://vim.fandom.com/wiki/Highlight_current_line
 local cursorGrp = vim.api.nvim_create_augroup('CursorLine', { clear = true })
 vim.api.nvim_create_autocmd(
-	{ 'InsertLeave', 'WinEnter' },
-	{ pattern = '*', command = 'set cursorline', group = cursorGrp }
+		{ 'InsertLeave', 'WinEnter' },
+		{ pattern = '*', command = 'set cursorline', group = cursorGrp }
 )
 vim.api.nvim_create_autocmd(
-	{ 'InsertEnter', 'WinLeave' },
-	{ pattern = '*', command = 'set nocursorline', group = cursorGrp }
+		{ 'InsertEnter', 'WinLeave' },
+		{ pattern = '*', command = 'set nocursorline', group = cursorGrp }
 )
 
 -- don't auto comment new line
--- nvim/.config/nvim/lua/goosetacob/lazy.lua
+++ nvim/.config/nvim/lua/goosetacob/lazy.lua
@@ -1,12 +1,12 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable', -- latest stable release
-		lazypath,
+			'git',
+			'clone',
+			'--filter=blob:none',
+			'https://github.com/folke/lazy.nvim.git',
+			'--branch=stable', -- latest stable release
+			lazypath,
 	})
 end
 vim.opt.rtp:prepend(lazypath)
@@ -15,100 +15,100 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 require 'lazy'.setup({
-	-- lsp
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		dependencies = {
-			-- LSP Support
-			{ 'neovim/nvim-lspconfig' },
-			{ 'williamboman/mason.nvim' },
-			{ 'williamboman/mason-lspconfig.nvim' },
-			{ 'jose-elias-alvarez/null-ls.nvim' },
-			{ 'jay-babu/mason-null-ls.nvim' },
+		-- lsp
+		{
+				'VonHeikemen/lsp-zero.nvim',
+				dependencies = {
+						-- LSP Support
+						{ 'neovim/nvim-lspconfig' },
+						{ 'williamboman/mason.nvim' },
+						{ 'williamboman/mason-lspconfig.nvim' },
+						{ 'jose-elias-alvarez/null-ls.nvim' },
+						{ 'jay-babu/mason-null-ls.nvim' },
 
-			-- Useful lsp status updates
-			{ 'j-hui/fidget.nvim' },
+						-- Useful lsp status updates
+						{ 'j-hui/fidget.nvim' },
 
-			-- Additional lua configuration, makes nvim stuff amazing
-			{ 'folke/neodev.nvim' },
+						-- Additional lua configuration, makes nvim stuff amazing
+						{ 'folke/neodev.nvim' },
 
-			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' },
-			{ 'hrsh7th/cmp-buffer' },
-			{ 'hrsh7th/cmp-path' },
-			{ 'saadparwaiz1/cmp_luasnip' },
-			{ 'hrsh7th/cmp-nvim-lsp' },
-			{ 'hrsh7th/cmp-nvim-lua' },
+						-- Autocompletion
+						{ 'hrsh7th/nvim-cmp' },
+						{ 'hrsh7th/cmp-buffer' },
+						{ 'hrsh7th/cmp-path' },
+						{ 'saadparwaiz1/cmp_luasnip' },
+						{ 'hrsh7th/cmp-nvim-lsp' },
+						{ 'hrsh7th/cmp-nvim-lua' },
 
-			-- Snippets
-			{ 'L3MON4D3/LuaSnip' },
-			{ 'rafamadriz/friendly-snippets' },
-		}
-	},
-
-	-- treesitter
-	{ 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-	'nvim-treesitter/playground',
-	'nvim-treesitter/nvim-treesitter-textobjects',
-	'p00f/nvim-ts-rainbow',
-
-	-- telescope
-	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.0',
-		-- or branch = '0.1.x',
-		dependencies = {
-			{ 'nvim-lua/plenary.nvim' },
-			{ 'nvim-lua/popup.nvim' }
-		}
-	},
-	'nvim-telescope/telescope-fzy-native.nvim',
-
-	-- extra language stuffs
-	'simrat39/rust-tools.nvim',
-
-	-- text manipulation
-	{
-		'numToStr/Comment.nvim',
-		dependencies = {
-			{ 'JoosepAlviste/nvim-ts-context-commentstring' },
+						-- Snippets
+						{ 'L3MON4D3/LuaSnip' },
+						{ 'rafamadriz/friendly-snippets' },
+				}
 		},
-		config = function()
-			require 'Comment'.setup({
-				pre_hook = require 'ts_context_commentstring.integrations.comment_nvim'.create_pre_hook(),
-			})
-		end
-	},
 
-	-- git stuff
-	'tpope/vim-fugitive',
-	'tpope/vim-rhubarb',
-	'lewis6991/gitsigns.nvim',
+		-- treesitter
+		{ 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+		'nvim-treesitter/playground',
+		'nvim-treesitter/nvim-treesitter-textobjects',
+		'p00f/nvim-ts-rainbow',
 
-	-- nice ui stuff
-	'hoob3rt/lualine.nvim',
-	'norcalli/nvim-colorizer.lua',
-	'airblade/vim-gitgutter',
+		-- telescope
+		{
+				'nvim-telescope/telescope.nvim',
+				tag = '0.1.0',
+				-- or branch = '0.1.x',
+				dependencies = {
+						{ 'nvim-lua/plenary.nvim' },
+						{ 'nvim-lua/popup.nvim' }
+				}
+		},
+		'nvim-telescope/telescope-fzy-native.nvim',
 
-	-- nice tools / settings
-	'mbbill/undotree',
-	'tpope/vim-sensible',
-	'tpope/vim-surround',
-	'tpope/vim-sleuth',
+		-- extra language stuffs
+		'simrat39/rust-tools.nvim',
 
-	-- extra stuff from vimscript config
-	-- not sure if i need it?
-	-- Plug 'ray-x/navigator.lua' " looks sweet, but can't figure out setup...
-	-- Plug 'ekalinin/Dockerfile.vim'
-	-- Plug 'evanleck/vim-svelte', {'branch': 'main'}
-	-- Plug 'rust-lang/rust.vim'
+		-- text manipulation
+		{
+				'numToStr/Comment.nvim',
+				dependencies = {
+						{ 'JoosepAlviste/nvim-ts-context-commentstring' },
+				},
+				config = function()
+					require 'Comment'.setup({
+							pre_hook = require 'ts_context_commentstring.integrations.comment_nvim'.create_pre_hook(),
+					})
+				end
+		},
 
-	-- colors
-	{
-		'rose-pine/neovim',
-		as = 'rose-pine',
-		config = function()
-			vim.cmd('colorscheme rose-pine')
-		end
-	},
+		-- git stuff
+		'tpope/vim-fugitive',
+		'tpope/vim-rhubarb',
+		'lewis6991/gitsigns.nvim',
+
+		-- nice ui stuff
+		'hoob3rt/lualine.nvim',
+		'norcalli/nvim-colorizer.lua',
+		'airblade/vim-gitgutter',
+
+		-- nice tools / settings
+		'mbbill/undotree',
+		'tpope/vim-sensible',
+		'tpope/vim-surround',
+		'tpope/vim-sleuth',
+
+		-- extra stuff from vimscript config
+		-- not sure if i need it?
+		-- Plug 'ray-x/navigator.lua' " looks sweet, but can't figure out setup...
+		-- Plug 'ekalinin/Dockerfile.vim'
+		-- Plug 'evanleck/vim-svelte', {'branch': 'main'}
+		-- Plug 'rust-lang/rust.vim'
+
+		-- colors
+		{
+				'rose-pine/neovim',
+				as = 'rose-pine',
+				config = function()
+					vim.cmd('colorscheme rose-pine')
+				end
+		},
 })
