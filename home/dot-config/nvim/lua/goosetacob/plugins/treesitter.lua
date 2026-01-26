c3c4955806a2b4265fd929c9849092e30759272c
Gustavo Blanco <gustavo.jr.blanco@gmail.com>
2026-01-25 16:38:21 -0800

    update

--- home/dot-config/nvim/lua/goosetacob/plugins/treesitter.lua
+++ home/dot-config/nvim/lua/goosetacob/plugins/treesitter.lua
@@ -3,93 +3,65 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"nvim-treesitter/playground",
-			"nvim-treesitter/nvim-treesitter-textobjects",
-			"JoosepAlviste/nvim-ts-context-commentstring",
-		},
 		config = function()
-			-- import nvim-treesitter plugin
-			require("ts_context_commentstring").setup({})
-			local treesitter = require("nvim-treesitter.configs")
-
-			-- configure treesitter
-			treesitter.setup({ -- enable syntax highlighting
-				highlight = {
-					-- `false` will disable the whole extension
-					enable = true,
-					use_languagetree = true,
-
-					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-					-- Using this option may slow down your editor, and you may see some duplicate highlights.
-					-- Instead of true it can also be a list of languages
-					additional_vim_regex_highlighting = false,
-				},
-
-				-- enable indentation
-				indent = { enable = true },
-
-				-- Install parsers synchronously (only applied to `ensure_installed`)
-				sync_install = true,
-
-				-- Automatically install missing parsers when entering buffer
-				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-				auto_install = true,
-
-				-- ensure these language parsers are installed
+			require("nvim-treesitter").setup({
 				ensure_installed = {
 					"bash",
+					"c",
 					"dockerfile",
+					"javascript",
 					"json",
 					"lua",
 					"markdown",
-					-- "markdown_inline",
-					"tsx",
-					"yaml",
-					"c",
-					"javascript",
-					"lua",
 					"python",
+					"tsx",
 					"typescript",
 					"vim",
+					"yaml",
 				},
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true, -- automatically jump forward to matching textobj
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-						},
-					},
-					swap = {
-						enable = true,
-						swap_next = { ["<leader>a"] = "@parameter.inner" },
-						swap_previous = { ["<leader>A"] = "@parameter.inner" },
-					},
-				},
-				playground = {
-					enable = true,
-					disable = {},
-					updatetime = 25,
-					persist_queries = false,
-					keybindings = {
-						toggle_query_editor = "o",
-						toggle_hl_groups = "i",
-						toggle_injected_languages = "t",
-						toggle_anonymous_nodes = "a",
-						toggle_language_display = "I",
-						focus_language = "f",
-						unfocus_language = "F",
-						update = "R",
-						goto_node = "<cr>",
-						show_help = "?",
-					},
-				},
+				auto_install = true,
+			})
+
+			-- Enable treesitter highlighting
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					pcall(vim.treesitter.start)
+				end,
 			})
 		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					lookahead = true,
+				},
+				move = {
+					set_jumps = true,
+				},
+			})
+
+			-- Select keymaps
+			local select = require("nvim-treesitter-textobjects.select")
+			vim.keymap.set({ "x", "o" }, "af", function() select.select_textobject("@function.outer", "textobjects") end)
+			vim.keymap.set({ "x", "o" }, "if", function() select.select_textobject("@function.inner", "textobjects") end)
+			vim.keymap.set({ "x", "o" }, "ac", function() select.select_textobject("@class.outer", "textobjects") end)
+			vim.keymap.set({ "x", "o" }, "ic", function() select.select_textobject("@class.inner", "textobjects") end)
+
+			-- Swap keymaps
+			local swap = require("nvim-treesitter-textobjects.swap")
+			vim.keymap.set("n", "<leader>a", function() swap.swap_next("@parameter.inner") end)
+			vim.keymap.set("n", "<leader>A", function() swap.swap_previous("@parameter.inner") end)
+		end,
+	},
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		lazy = true,
+		opts = {
+			enable_autocmd = false,
+		},
 	},
 }
